95aee8662b265278816f4dd9043c4d912da84997
Tulasisree <sree.mulagapaka@gmail.com>
2026-07-13 20:54:39 +0530

    Basic Consumer Producer

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer;
+
+public class Receiver
+{
+    public static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory(){HostName = "localhost"}; 
+        await using var connection = await factory.CreateConnectionAsync();
+        await using var channel = await connection.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync("BasicTest", true, false, false,null);
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+
+        consumer.ReceivedAsync += async (channel, a) =>
+        {
+            var body = a.Body.ToArray();//gestring expects a byte[]
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine("Receiver message {0}",message);
+            await Task.CompletedTask;
+        };
+
+        //consuming message
+        //"true" -> auto acknowledge
+        await channel.BasicConsumeAsync("BasicTest",true,consumer);
+
+        Console.WriteLine("Press [enter] to exit the Consumer App..");
+        Console.ReadLine();
+    }
+}
+
---
+++ Producer/Sender.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+public class Sender
+{
+    public static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory(){HostName = "localhost"}; //hostname -> where rabbitmq is running
+        //using is used for disposal and awaiting on that
+        await using var connection = await factory.CreateConnectionAsync(); //open connection
+        //in 7.x createModel() replace with CreateChannelAsync()
+        await using var channel = await connection.CreateChannelAsync(); //channel to create queue , pubkush message and close
+        
+        //queue name is Basci Test
+        await channel.QueueDeclareAsync("BasicTest", true, false, false, null);
+
+        string message = "Getting started with .NET Core RabbitMQ";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        await channel.BasicPublishAsync("", "BasicTest", body);
+        Console.WriteLine("Sent messagw {0}...",message);
+
+        Console.WriteLine("Press [enter] to exit the Sender App..");
+        Console.ReadLine();
+    }
+}
+
+// QueueDeclare(
+//     "hello",
+//     false,
+//     false,
+//     false,
+//     null);
+
+// That worked in RabbitMQ 3.x.
+
+// You're using:
+
+// RabbitMQ Server 4.x
+// RabbitMQ.Client 7.2.1
+
+// RabbitMQ 4.x introduced several breaking changes and deprecated transient non-exclusive queues.
